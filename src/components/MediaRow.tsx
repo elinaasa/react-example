13309b03247bf435a694d0951dbeb4f5514e9075
Elina <sankilaelina@gmail.com>
2024-02-12 16:43:31 +0200

    build added

---
+++ src/components/MediaRow.tsx
@@ -0,0 +1,27 @@
+import {Link} from 'react-router-dom';
+import {MediaItemWithOwner} from '../types/DBTypes';
+
+const MediaRow = (props: {
+  item: MediaItemWithOwner
+}) => {
+  const {item}  = props;
+
+  return (
+    <tr className="media-row">
+      <td>
+        <img src={item.thumbnail} alt={item.title} />
+      </td>
+      <td>{item.title}</td>
+      <td>{item.description}</td>
+      <td>{new Date(item.created_at).toLocaleString('fi-FI')}</td>
+      <td>{item.filesize}</td>
+      <td>{item.media_type}</td>
+      <td>{item.username}</td>
+      <td>
+        <Link to="/single" state={item}>View</Link>
+      </td>
+    </tr>
+  );
+};
+
+export default MediaRow;
